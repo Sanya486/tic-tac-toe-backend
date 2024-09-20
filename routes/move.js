6f3bed2f1915220498cc6f19554a8c87694b10ec
Sanya486 <sanyok486@gmail.com>
2024-09-20 16:55:02 +0300

    initiate server

---
+++ routes/move.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/', async (req, res) => {
+    res.json({
+        message: "Hello World"
+    })
+});
+
+
+module.exports = router;
